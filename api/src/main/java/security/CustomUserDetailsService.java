02a0070cbeb72f767ec2760ad5d9bdfbaa7a860a
PedroAntonio <antonio.pedro25@outlook.com>
2025-01-07 14:02:41 -0300

    Customizacao do UserDetails para usar o UserModel

--- api/src/main/java/security/CustomUserDetailsService.java
+++ api/src/main/java/security/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package security;
 
-public class CustomUserDetailsService {
+import io.github.api.domain.UserModel;
+import io.github.api.service.UserModelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserModelService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        UserModel user = service.getByLogin(login);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return User.builder()
+                .username(user.getLogin())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[user.getRoles().size()]))
+                .build();
+    }
 }
