5ce1e8d6da49429e75897d70c33954d7ca637e80
PedroAntonio <antonio.pedro25@outlook.com>
2025-01-08 17:41:27 -0300

    Configuracao do Hikari

---
+++ api/src/main/java/io/github/api/config/DatabaseConfig.java
@@ -0,0 +1,46 @@
+package io.github.api.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+
+    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setJdbcUrl(url);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("ecommercedb-pool");
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000);
+        config.setConnectionTestQuery("SELECT 1");
+
+        return new HikariDataSource(config);
+    }
+}
